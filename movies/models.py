9bd4e7e475504116c45a0609fa3f0963c278d13f
Luka929 <Luka929@example.com>
2025-12-18 01:38:26 -0500

    Add favorites endpoints and models

--- movies/models.py
+++ movies/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.conf import settings
+
 
 class Movie(models.Model):
     title = models.CharField(max_length=255)
@@ -8,4 +10,23 @@ class Movie(models.Model):
 
     def __str__(self):
         return self.title
+
+class Favorite(models.Model):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='favorites'
+    )
+    movie = models.ForeignKey(
+        'Movie',
+        on_delete=models.CASCADE,
+        related_name='favorited_by'
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ('user', 'movie')
+
+    def __str__(self):
+        return f"{self.user} → {self.movie}"
 
--- movies/views.py
+++ movies/views.py
@@ -2,9 +2,28 @@ from rest_framework.viewsets import ModelViewSet
 from .models import Movie
 from .serializers import MovieSerializer
 from .permissions import IsAdminOrReadOnly
+from rest_framework import generics, permissions
+from .models import Favorite
+from .serializers import FavoriteSerializer
 
 class MovieViewSet(ModelViewSet):
     queryset = Movie.objects.all()
     serializer_class = MovieSerializer
     permission_classes = [IsAdminOrReadOnly]
 
+class FavoriteListCreateView(generics.ListCreateAPIView):
+    serializer_class = FavoriteSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        return Favorite.objects.filter(user=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+class FavoriteDetailView(generics.RetrieveDestroyAPIView):
+    serializer_class = FavoriteSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        return Favorite.objects.filter(user=self.request.user)
