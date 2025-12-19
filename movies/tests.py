368ae4e192ae002bce23608ef0dc2863767cbf1c
Luka929 <Luka929@example.com>
2025-12-19 04:23:42 -0500

    Add latest test changes

--- movies/tests.py
+++ movies/tests.py
@@ -1,3 +1,24 @@
-from django.test import TestCase
+import pytest
+from django.urls import reverse
+from django.test import Client
+from movies.models import Movie
 
-# Create your tests here.
+@pytest.mark.django_db
+def test_movie_creation():
+    movie = Movie.objects.create(
+        title="Inception",
+        description="A mind-bending thriller",
+        release_year=2010
+    )
+    assert movie.title == "Inception"
+    assert movie.release_year == 2010
+
+
+@pytest.mark.django_db
+def test_movie_list_view():
+    client = Client()
+    url = reverse("movies:list")
+    response = client.get(url)
+    assert response.status_code == 200
+
+
--- movies/urls.py
+++ movies/urls.py
@@ -1,14 +1,17 @@
 from django.urls import path
 from rest_framework.routers import DefaultRouter
-from .views import MovieViewSet, FavoriteListCreateView, FavoriteDetailView, CommentListCreateView, CommentDeleteView
+from .views import MovieViewSet, FavoriteListCreateView, FavoriteDetailView, CommentListCreateView, CommentDeleteView, \
+    movie_list_view
 
 router = DefaultRouter()
 router.register("movies", MovieViewSet)
+app_name = 'movies'
 
 urlpatterns = router.urls + [
     path('favorites/', FavoriteListCreateView.as_view()),
     path('favorites/<int:pk>/', FavoriteDetailView.as_view()),
     path('movies/<int:movie_id>/comments/', CommentListCreateView.as_view()),
     path('comments/<int:pk>/delete/', CommentDeleteView.as_view()),
+    path('', movie_list_view, name='list'),
 ]
 
